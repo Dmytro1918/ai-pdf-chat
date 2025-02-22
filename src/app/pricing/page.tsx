2b3744a92d60861a0956637b580d2045694c4a4a
dmytro.kameniev <dmytro.kameniev@valtech.com>
2025-02-22 18:24:47 +0200

    Pricing page update

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link"
 import MaxWidthWrapper from "../components/maxWidthWrapper"
 import { buttonVariants } from "@/components/ui/button"
+import { FcNext } from "react-icons/fc";
 
 
 export default function Pricing () {
     return (
     <MaxWidthWrapper className='mt-12 mb-28 sm:mt-14 flex flex-col justify-center items-center text-center'>
         <h1 className=" mb-16 max-w-4xl  text-5xl font-bold md:text-6xl lg:text-7xl ">
-            Choose your subscription and enjoy 30 days for FREE!
+            Choose your subscription and enjoy 30 days for <span className="text-blue-600">FREE!</span>
         </h1>
-        <div className=" pt-10 w-full flex flex-row items-center text-center justify-around ">
+        <div className=" pt-10 w-full flex flex-row text-center justify-around items-stretch">
             <div className=" pt-10 pb-10 w-full flex-column justify-center items-center text-center max-h-screen border-4 rounded-xl border-gray-500 ">
                 <h2 className="text-center font-semibold text-4xl mb-8">
                     Easy
@@ -27,8 +28,22 @@ export default function Pricing () {
                     })}>
                     Get 30 days for free
                 </Link>
+                <div className=" m-10 flex flex-col indent-3 gap-3">
+                    <p className="flex items-center justify-start  underline underline-offset-8 fade-in"> 
+                        <FcNext/> Up to 30 requests per month
+                    </p>
+                    <p className="flex items-center justify-start  underline underline-offset-8 fade-in"> 
+                        <FcNext/> Up to 10 PDF uploads per month
+                    </p>
+                    <p className="flex items-center justify-start underline underline-offset-8 fade-in"> 
+                        <FcNext/> Service may include adds
+                    </p>
+                    <p className="flex items-center justify-start underline underline-offset-8 fade-in"> 
+                        <FcNext/> Email support 24 hours
+                    </p>
+                </div>
             </div>
-            <div className="pt-10 pb-10 w-full flex-column justify-center items-center text-center max-h-screen border-4 rounded-xl border-gray-500 ml-5">
+            <div className="pt-10 pb-10 w-full flex-column justify-center items-center text-center max-h-screen border-4 rounded-xl border-gray-500 ml-5 gap-3">
                 <h2 className="text-center font-semibold text-4xl mb-8">
                     Rookie
                 </h2>
@@ -45,8 +60,22 @@ export default function Pricing () {
                     })}>
                     Get 30 days for free
                 </Link>
+                <div className=" m-10 flex flex-col items-start indent-3 gap-3 ">
+                    <p className=" flex items-center justify-start underline underline-offset-8 fade-in"> 
+                        <FcNext/> Up to 50 requests per month
+                    </p>
+                    <p className="flex items-center justify-start underline underline-offset-8 fade-in"> 
+                        <FcNext/> Up to 20 PDF uploads per month
+                    </p>
+                    <p className="flex items-center justify-start underline underline-offset-8 fade-in"> 
+                        <FcNext/> No ads
+                    </p>
+                    <p className="flex items-center justify-start underline underline-offset-8 fade-in"> 
+                        <FcNext/> Email and chat support 24 hours
+                    </p>
+                </div>
             </div>
-            <div className="pt-10 pb-10 w-full flex-column justify-center items-center text-center max-h-screen border-4 rounded-xl border-gray-500 ml-5">
+            <div className="pt-10 pb-10 w-full flex-column justify-center items-center text-center max-h-screen border-4 rounded-xl border-gray-500 ml-5 gap-3">
                 <h2 className="text-center font-semibold text-4xl mb-8">
                    Veteran
                 </h2>
@@ -63,6 +92,24 @@ export default function Pricing () {
                     })}>
                     Get 30 days for free
                 </Link>
+                <div className="my-10 mx-4 flex flex-col items-start indent-3 gap-3">
+                    <p className="flex items-center justify-start underline underline-offset-8 fade-in"> 
+                        <FcNext/> Up to 100 requests per month
+                    </p>
+                    <p className="flex items-center justify-start underline underline-offset-8 fade-in"> 
+                        <FcNext/> Unlimited PDF upload
+                    </p>
+                    <p className="flex items-center justify-start underline underline-offset-8 fade-in"> 
+                        <FcNext/> No ads
+                    </p>
+                    <p className="flex items-center justify-start underline underline-offset-8 fade-in" > 
+                        <FcNext/> Share PDFs and insights with team members
+                    </p>
+                    <p className="flex items-center justify-start underline underline-offset-8 fade-in"> 
+                        <FcNext/> Email and chat support with a 1-hour response time
+                    </p>
+                    
+                </div>
             </div>
         </div>
     </MaxWidthWrapper>
