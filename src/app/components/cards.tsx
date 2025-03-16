f530098866b4e17a1ba437bae64436013de4bda0
dmytro.kameniev <dmytro.kameniev@valtech.com>
2025-03-16 14:02:43 +0200

    Changed style for cards;

--- src/app/components/cards.tsx
+++ src/app/components/cards.tsx
@@ -14,16 +14,16 @@ export const Cards = ({ cardsList }:{ cardsList:CardsData[] }) => {
 
     return (
         <>
-                <div className=" grid grid-cols-2 gap-6 w-11/12 mt-20 mb-20 ">
+                <div className=" grid grid-cols-3 gap-12 max-w-max mt-20 mb-20 g-6/12">
                     {cardsList.map((item,index) => (
-                        <div className="border-2 border-solid border-blue-300 rounded-xl p-6 flex flex-col items-start h-full" key={index}>
-                            <h3>
+                        <div className="border-2 border-solid border-blue-300 rounded-xl p-6 flex flex-col items-start h-full transition-transform duration-300 ease-in-out hover:scale-110" key={index}>
+                            <h3 className="text-2xl font-thin p-2 self-center">
                                 {item.title}
                             </h3>
-                            <p>
+                            <p className="text-xl font-thin p-2">
                                 {item.description}
                             </p>
-                            <div className="w-full flex justify-center mt-8"> 
+                            <div className="w-full flex justify-center mt-8 "> 
                                 <Image 
                                     src={item.imageSrc} 
                                     alt={item.imageAlt} 
